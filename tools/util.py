ffcbc7a47af9727d2c59851b9947f50a4c0f59ca
Zed630 <145366947+Zed630@users.noreply.github.com>
2025-06-06 16:31:36 +0800

    Update util.py

--- tools/util.py
+++ tools/util.py
@@ -17,10 +17,7 @@ def load_prompts(llmname="llama-2"):
 
     instences = []
     for i in range(len(harmful_prompts)):
-        if llmname == "llama-3":
-            instences.append(Instence(harmful_prompts[i] + ". ", harmful_targets[i].replace("Sure, h", "H")))
-        else:
-            instences.append(Instence(harmful_prompts[i] + " ", harmful_targets[i]))
+        instences.append(Instence(harmful_prompts[i] + " ", harmful_targets[i]))
     return instences
 
 def save_to_jsonl(result, full_path='./result/data.jsonl'):
@@ -40,7 +37,7 @@ def gcg_loss(logits, labels):
     shift_logits = shift_logits.transpose(1, 2)  # B * V * (L-1)
     shift_labels = labels[:, 1:].contiguous()  # B * (L-1)
     masked_loss = F.cross_entropy(shift_logits, shift_labels,
-                                  reduction='none')  # B * (L-1) # CrossEntropyLoss会自动把label为-100的loss置为0
+                                  reduction='none')  # B * (L-1) 
 
     mask = (shift_labels != -100)
     valid_elements_per_row = mask.sum(dim=1)  # B
@@ -64,11 +61,10 @@ def gcg_origin(model, instences, config, case_idx):
     aver_target = torch.load(f"./{model.llm_name}/test_{layer}.pth").to(model.device).to(model.target_model.dtype)
     origin = torch.load(f"./{model.llm_name}/test_{layer}_h.pth").to(model.device).to(model.target_model.dtype)
     direction = aver_target - origin
-    # 初始化越狱后缀
+    # Initialize suffix
     #suffix_ids = torch.randint(0, V, (1, suffix_length)).to(model.device)
     suffix_ids = torch.tensor(model.tokenizer.encode("! " * suffix_length, add_special_tokens=False)[:suffix_length]).view(1, -1).to(model.device)
 
-    #计算时间开销
     total_start = time.time()
     for inst in instences:
         ids, prompt = model.construct_inputs(inst.prompt, sys="")
@@ -76,20 +72,19 @@ def gcg_origin(model, instences, config, case_idx):
         endtoken_length = max([i if prompt[-i:] == prompt_temp[-i:] else 0 for i in range(len(prompt))])
         ids = ids.to(model.device)
         prompt_len = len(prompt)
-        #添加分割符号
 
         """if model.llm_name == "llama-3":
             ids = torch.cat([ids[:, :-model.endtoken_length], torch.tensor([256]).view(1, -1).to(model.device), ids[:, -model.endtoken_length:]], dim=1)
             prompt_len += 1"""
-        inst.L1 = prompt_len - endtoken_length  # 第一个越狱字符位置
+        inst.L1 = prompt_len - endtoken_length  # Suffix index
         outputs = model.target_model(input_ids=ids.view(1, -1), output_hidden_states=True)
         inst.origin = outputs.hidden_states[layer][0][-1].detach().clone()
         inst.semantic = outputs.hidden_states[18][0][-2].detach().clone()
-        # 加上越狱后缀，初始化越狱token
+        # Appending suffix
         ids = torch.cat([ids[:, :inst.L1], suffix_ids, ids[:, inst.L1:]], dim=1)
         inst.ids = ids
         inst.L2 = ids.size(1)
-        #加上答案的ids, llama-3从0开始，llama2从1开始
+        # Appending target
         ans_ids = torch.tensor(model.tokenizer.encode(inst.target, add_special_tokens=False)[:suffix_length]).view(1, -1).to(model.device)
         refuse_ids = torch.tensor(model.tokenizer.encode(" I cannot fulfill", add_special_tokens=False)[:suffix_length]).view(1, -1).to(model.device)
         inst.whole_ids = torch.cat([ids, ans_ids], dim=1).clone()
@@ -102,12 +97,12 @@ def gcg_origin(model, instences, config, case_idx):
         labels[:, inst.L2:] = inst.refuse_ids[:, inst.L2:]
         inst.refuse_labels = labels.clone()
 
-    # 评估和迭代
+    # Iteration and Evaluation
     gradient_cost = 0
     batch_cost = 0
     evaluation_cost = 0
     for i in range(config.max_epochs):
-        # 构建独热矩阵输入
+        # construct input
         totol_grad = 0
 
         start_time = time.time()
@@ -117,8 +112,7 @@ def gcg_origin(model, instences, config, case_idx):
             embed_matrix = embed.weight  # V * D
             inputs_embeds = torch.matmul(one_hot_input, embed_matrix)  # 1 * L * D
 
-            # 生成一次，找到梯度
-            # gcg的loss算法
+            # get gradient
             outputs = model.target_model(inputs_embeds=inputs_embeds, labels=inst.labels, output_hidden_states=True)
             loss = outputs.loss
             #loss = outputs.loss*alpha - (outputs.hidden_states[layer][0][inst.L2-1] - inst.origin).flatten() @ direction.flatten() / direction.norm()
@@ -135,21 +129,19 @@ def gcg_origin(model, instences, config, case_idx):
             totol_grad += token_grad
         gradient_cost += time.time() - start_time
 
-        # 选词
+        # choose substitude
         with torch.no_grad():
             start_time = time.time()
-            # 去掉不需要的tokens
+            # getoff tokens
             for token_ids in model.nonsense_ids:
                 totol_grad[:, :, token_ids] = float('-inf')
             top_k_indices = torch.topk(totol_grad, dim=2, k=suffix_num).indices
-            # 得到替换后的句子
 
             suffix_id_lst = []
             for _ in range(suffix_num):
                 rel_idx = random.randint(0, suffix_length - 1)
                 new_token_id = top_k_indices[0][rel_idx][random.randint(0, suffix_num/4 - 1)]
 
-                # 替换为suffix
                 temp = suffix_ids.clone()
                 temp[:, rel_idx] = new_token_id
                 suffix_id_lst.append(temp)
@@ -157,7 +149,7 @@ def gcg_origin(model, instences, config, case_idx):
             total_loss = 0
 
             for inst in instences:
-                # 分batch
+                # batch
                 if len(suffix_id_lst) > batch_size:
                     batches = [suffix_id_lst[i: i + batch_size] for i in range(0, len(suffix_id_lst), batch_size)]
                 else:
@@ -166,17 +158,16 @@ def gcg_origin(model, instences, config, case_idx):
                 for batch in batches:
                     batch_ids = []
                     for suffix in batch:
-                        # 加上越狱后缀，初始化越狱token
                         ids = torch.cat([inst.ids[:, :inst.L1], suffix, inst.whole_ids[:, inst.L1+suffix_length:]], dim=1)
                         batch_ids.append(ids)
 
                     torch.cuda.empty_cache()
-                    # 选bestone
+                    # get bestone
                     # gcg_batch_loss
                     outputs = model.target_model(input_ids=torch.cat(batch_ids), output_hidden_states=True)
                     logits = outputs.logits
-                    temp = gcg_loss(logits, torch.cat([inst.labels] * len(batch)))
-                    #temp = gcg_loss(logits, torch.cat([inst.labels]*len(batch)))*alpha - (outputs.hidden_states[layer][:, inst.L2-1] - inst.origin).view(-1, 4096) @ direction.flatten() / direction.norm()
+                    #temp = gcg_loss(logits, torch.cat([inst.labels] * len(batch)))
+                    temp = gcg_loss(logits, torch.cat([inst.labels]*len(batch)))*alpha - (outputs.hidden_states[layer][:, inst.L2-1] - inst.origin).view(-1, 4096) @ direction.flatten() / direction.norm()
                     #temp = gcg_loss(logits, torch.cat([inst.labels] * len(batch)))
                     batch_loss = torch.cat([batch_loss, temp])
                 total_loss += batch_loss
@@ -187,19 +178,17 @@ def gcg_origin(model, instences, config, case_idx):
             _pass = 0
 
             for inst in instences:
-                # 加上越狱后缀，初始化越狱token
                 ids = torch.cat([inst.ids[:, :inst.L1], suffix_ids, inst.ids[:, inst.L1+suffix_length:]], dim=1)
                 inst.whole_ids[:, inst.L1:inst.L1+suffix_length] = suffix_ids
                 inst.refuse_ids[:, inst.L1:inst.L1 + suffix_length] = suffix_ids
-                #是否评估并保存结果
+                #Evaluation and Save
                 if config.generate_response:
                     model.generate_by_token_ids(ids, length=100)
                     if early_stopping and eval.llm_detector(inst.prompt, model.generate_text, config):
                         _pass += 1
                     whole_prompt = model.tokenizer.decode(ids[0])
-                    #评估时间开销
                     evaluation_cost += time.time() - start_time
-                    # 保存结果
+                    # save result
                     data = {
                         'request': inst.prompt,
                         "prompt": ids.tolist(),
